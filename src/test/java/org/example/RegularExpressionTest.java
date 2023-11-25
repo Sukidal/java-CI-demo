d0b91074c11c66fbf108b2fbfc0b51cd4dba782a
Sukidal <2634968536@qq.com>
2023-11-25 21:43:42 +0800

    finish action.yml

---
+++ src/test/java/org/example/RegularExpressionTest.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import junit.framework.TestCase;
+
+public class RegularExpressionTest extends TestCase {
+
+    /*
+        test for RegularExpression
+     */
+    public void testRegularExpression() {
+
+    }
+}
